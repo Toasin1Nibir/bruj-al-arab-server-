a3f72fcb2c241b8ca9a0d2b5ec6255553aa71fa6
NIBIR <ntoasin@gmail.com>
2021-03-29 11:50:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const port = 5000
+const pass ='arabianhorse79'
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://arabian:arabianhorse79@cluster0.u3b1e.mongodb.net/burjAlArab?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const bookings = client.db("burjAlArab").collection("bookings");
+  // perform actions on the collection object
+  // console.log('success')
+  // client.close();
+
+  app.post('/addBooking', (req,res) => {
+    const newbooking = req.body;
+    bookings.insertOne(newbooking)
+    .then(result => {
+      res.send(result.insertedCount > 0)
+    })
+    console.log(newbooking)
+  })
+
+
+app.get('/bookings',(req,res) =>{
+  bookings.find({})
+  .toArray((err , documents) =>{
+    res.send(documents)
+  })
+
+  
+})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port)
