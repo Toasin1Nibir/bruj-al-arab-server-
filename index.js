660c02802a6c0f52a81080688f0bd9d0a42533ae
NIBIR <ntoasin@gmail.com>
2021-03-29 12:09:45 +0600

    first commi

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ app.use(bodyParser.json())
 
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://arabian:arabianhorse79@cluster0.u3b1e.mongodb.net/burjAlArab?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://arabian:arabianhorse79@cluster0.u3b1e.mongodb.net/burjAlArab?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_Pass}@cluster0.u3b1e.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const bookings = client.db("burjAlArab").collection("bookings");
@@ -54,4 +57,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port)
+app.listen(process.env.PORT || port)
